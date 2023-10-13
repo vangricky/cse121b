7d1fed76076f82b1bc521fad3d3ed7475772f57f
Ricky Vang <vangcheemeng@yahoo.com>
2023-10-12 22:11:32 -0600

    fetch

--- week05/ponder-activities/Fetch Basics/fetch.js
+++ week05/ponder-activities/Fetch Basics/fetch.js
@@ -1,6 +1,9 @@
 // fetch.js
 const url = "https://pokeapi.co/api/v2/pokemon/ditto";
+const outputList = document.querySelector("#outputList");
+const urlList = "https://pokeapi.co/api/v2/pokemon";
 let results = null;
+
 async function getPokemon(url) {
   const response = await fetch(url);
   //check to see if the fetch was successful
@@ -11,13 +14,34 @@ async function getPokemon(url) {
     doStuff(data);
   }
 }
+
+async function getPokemonList(url) {
+  const response = await fetch(url);
+  if (response.ok) {
+    const data = await response.json();
+    doStuffList(data);
+  }
+}
+
 function doStuff(data) {
-  const output = document.querySelector("#output");
-  const results = data;
+  results = data;
   const html = `<h2>${results.name}</h2>
-  <img src="${results.sprites.front_default}" alt="Image of ${results.name}">`;
-  output.innerHTML = html;
+                <img src="${results.sprites.front_default}" alt="Image of ${results.name}">`;
+  outputElement.innerHTML = html;
   console.log("first: ", results);
 }
+
+function doStuffList(data) {
+  console.log(data);
+  const pokeListElement = document.querySelector("#outputList");
+  const pokeList = data.results;
+  pokeList.forEach((currentItem) => {
+    const html = `<li>${currentItem.name}</li>`;
+    pokeListElement.innerHTML += html;
+  });
+}
+
 getPokemon(url);
 console.log("second: ", results);
+
+getPokemonList(urlList);
