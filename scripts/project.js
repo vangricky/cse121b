58e22f1b2740be00b75c991cdde65e9d59cec7cd
Ricky Vang <vangcheemeng@yahoo.com>
2023-10-23 23:50:24 -0600

    js

--- scripts/project.js
+++ scripts/project.js
@@ -1,14 +1,10 @@
-// Module to fetch and display random dog pictures
 const dogAPI = {
-  apiKey: "YOUR_API_KEY", // Replace with your actual API key from a dog picture API
-
-  // Function to fetch a random dog picture
   async fetchRandomDog() {
     const apiUrl = `https://dog.ceo/api/breeds/image/random`;
     try {
       const response = await fetch(apiUrl);
       const data = await response.json();
-      return data.message; // Returns the URL of the random dog picture
+      return data.message;
     } catch (error) {
       console.error("Error fetching data:", error);
       return null;
@@ -16,7 +12,6 @@ const dogAPI = {
   },
 };
 
-// Function to handle the button click and display a random dog picture
 async function handleButtonClick() {
   const dogImage = document.getElementById("dog-pic");
   const randomDogImageURL = await dogAPI.fetchRandomDog();
@@ -24,11 +19,10 @@ async function handleButtonClick() {
   if (randomDogImageURL) {
     dogImage.src = randomDogImageURL;
   } else {
-    dogImage.src = ""; // Clear the image if there's an error
+    dogImage.src = "";
   }
 }
 
-// Event listener for the button click
 document
   .getElementById("get-dog-button")
   .addEventListener("click", handleButtonClick);
